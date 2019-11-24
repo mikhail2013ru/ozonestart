689b66a0916f3088e0f59cab78b631a6869d5815
mikhail2013ru <mikhail2013ru@gmail.com>
2019-11-24 15:09:54 +0200

    project update
    project update

---
+++ src/index.js
@@ -0,0 +1,20 @@
+'use strict';
+
+import getData from './modules/getData';
+import renderCards from './modules/renderCards';
+import renderCatalog from './modules/renderCatalog';
+import toggleCheckbox from './modules/toggleCheckbox';
+import toggleCart from './modules/toggleCart';
+import addCart from './modules/addCart';
+import actionPage from './modules/actionPage';
+
+(async function() {
+	const db = await getData();
+	renderCards(db);
+	renderCatalog();
+	toggleCheckbox();
+	toggleCart();
+	addCart();
+	actionPage();
+}());
+
